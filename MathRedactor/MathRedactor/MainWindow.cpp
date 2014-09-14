e183d817ef2649342fc82ab775174f7a26797dfb
Valeriy <stelz40494@gmail.com>
2014-09-14 15:37:02 +0400

    Added project with empty window
    Добавлен проект с пустым окном. С Ribbon'ом пока некоторые затруднения,
    как только допишу - залью

---
+++ MathRedactor/MathRedactor/MainWindow.cpp
@@ -0,0 +1,76 @@
+﻿#include "MainWindow.h"
+
+// класс CMainWindow
+// public методы 
+
+CMainWindow::CMainWindow() 
+{
+	windowHandle = NULL;
+}
+
+CMainWindow::~CMainWindow()
+{
+}
+
+bool CMainWindow::RegisterClass( HINSTANCE classOwnerInstance )
+{
+	const wchar_t* className = L"MathRedactorWindowClass";
+
+	WNDCLASSEX classInfo;
+	::ZeroMemory( &classInfo, sizeof( WNDCLASSEX ) );
+	classInfo.cbSize = sizeof( WNDCLASSEX );
+	classInfo.hInstance = classOwnerInstance;
+	classInfo.lpszClassName = className;
+	classInfo.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	classInfo.lpfnWndProc = &CMainWindow::windowProcedure;
+
+	if( ::RegisterClassEx( &classInfo ) == 0 ) {
+		return false;
+	}
+
+	return true;
+}
+
+bool CMainWindow::Create( LPCWSTR windowName, HINSTANCE ownerInstance )
+{
+	const wchar_t* className = L"MathRedactorWindowClass";
+
+	DWORD style = WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_VSCROLL | WS_HSCROLL;
+	windowHandle = ::CreateWindowEx( 0, className, windowName, style, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, ownerInstance, this );
+
+	if( windowHandle == NULL ) {
+		return false;
+	}
+
+	return true;
+}
+
+void CMainWindow::Show( int nCmdShow ) 
+{
+	::ShowWindow( windowHandle, nCmdShow );
+}
+
+// protected методы
+
+void CMainWindow::OnWmDestroy() {
+	::PostQuitMessage( 0 );
+}
+
+// private методы
+
+// процедура обабатывающая сообщения для главного окна
+LRESULT __stdcall CMainWindow::windowProcedure( HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam )
+{
+	if( message == WM_NCCREATE ) {
+		::SetWindowLong( windowHandle, GWL_USERDATA, reinterpret_cast<LONG>( reinterpret_cast<CREATESTRUCT*>( lParam )->lpCreateParams ) );
+	}
+
+	CMainWindow* window = reinterpret_cast<CMainWindow*>( ::GetWindowLong( windowHandle, GWL_USERDATA ) );	
+
+	switch( message ) {
+	case WM_CLOSE:
+		window->OnWmDestroy();
+	}
+
+	return ::DefWindowProc( windowHandle, message, wParam, lParam );
+}
---
+++ MathRedactor/MathRedactor/MainWindow.h
@@ -0,0 +1,29 @@
+﻿// Автор: Федюнин Валерий
+// Описание: класс главного окна приложения
+
+#include <Windows.h>
+
+// Класса, соответствующий главному окну приложения
+class CMainWindow {
+public:
+	CMainWindow();
+	~CMainWindow();
+
+	// регистрирует класс окна, 
+	static bool RegisterClass( HINSTANCE );
+	
+	// Создает окно
+	bool Create( LPCWSTR, HINSTANCE );
+	// Отображает окно
+	void Show( int );
+	
+protected:
+	// метод, вызываемый при получении окном сообщения WM_DESTROY
+	void OnWmDestroy();
+
+private:
+	// хэнджл окна, которому соответствуует этот объект класса.
+	HWND windowHandle;
+
+	static LRESULT __stdcall windowProcedure( HWND, UINT, WPARAM, LPARAM );
+};
